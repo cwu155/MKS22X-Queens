beb56c504ee8d826d73bb4c87c474b8fb81cb813
cwu155 <cwu17@stuy.edu>
2019-02-07 11:34:06 -0500

    Created QueenBoard.java

---
+++ QueenBoard.java
@@ -0,0 +1,33 @@
+public class QueenBoard{
+
+  private int[][]board;
+  private int n;
+
+  public QueenBoard(int size){
+    n = size;
+  }
+
+  // private boolean addQueen(int r, int c){}
+  // private boolean removeQueen(int r, int c){}
+
+  public String toString(){
+    String result = "";
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+          if (j == n-1){
+            result += "_" + "\n";
+          } else {
+            result += "_" + " ";
+            }
+          }
+        }
+        return result;
+      }
+
+    public static void main(String[] args) {
+      QueenBoard test = new QueenBoard(4);
+      System.out.println(test.toString());
+    }
+
+
+}
